02c84acb0e3e6d3f9be6f0ec4cdf05738ef23c7d
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-02-21 13:12:08 -0800

    worked on doing math with functions

---
+++ math.js
@@ -0,0 +1,33 @@
+
+
+
+//this is the function for addition
+function add() {
+    let sum = 1+2+3+4;
+    return sum;
+}
+
+console.log(add());
+
+//this is the code for my function for subtraction
+function subtraction() {
+    let subtraction = 25-12;
+    return subtraction;
+}
+
+console.log(subtraction());
+
+//this is my code function for multiplication
+function multiplication() {
+    let multiplication = 5*2*4;
+    return multiplication;
+}
+
+console.log(multiplication());
+//this is the division part
+function division() {
+    let division = 30/5;
+    return division;
+}
+
+console.log(division());
